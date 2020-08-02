9a8cdbeec0eeb764dd0d71e0457d7b6623fc7dcf
Adam Harley <aharley@cmu.edu>
2020-08-01 23:01:18 -0400

    fixed bug in detnet where wrong scorelist was used

--- model_carla_det.py
+++ model_carla_det.py
@@ -99,9 +99,7 @@ class CarlaDetModel(nn.Module):
         self.xyz_camXs = feed['xyz_camXs']
         self.xyz_camX0s = __u(utils.geom.apply_4x4(__p(self.camX0s_T_camXs), __p(self.xyz_camXs)))
         
-        if self.set_name=='test' or self.set_name=='val' or self.set_name=='train':
-            print('fixing centroid')
-            # fixed centroid
+        if self.set_name=='test' or self.set_name=='val':
             scene_centroid_x = 0.0
             scene_centroid_y = 1.0
             scene_centroid_z = 18.0
@@ -116,7 +114,7 @@ class CarlaDetModel(nn.Module):
             num_tries = 0
             while (not all_ok) and (num_tries < 100):
                 scene_centroid_x = np.random.uniform(-8.0, 8.0)
-                scene_centroid_y = np.random.uniform(-1.5, 3.0)
+                scene_centroid_y = np.random.uniform(-1.0, 3.0)
                 scene_centroid_z = np.random.uniform(10.0, 26.0)
                 scene_centroid = np.array([scene_centroid_x,
                                            scene_centroid_y,
@@ -136,7 +134,7 @@ class CarlaDetModel(nn.Module):
             self.summ_writer.summ_scalar('centroid_sampling/num_tries', float(num_tries))
             self.summ_writer.summ_scalar('centroid_sampling/num_inb', torch.mean(num_inb).cpu().item())
             if num_tries >= 100:
-                return False
+                return False # not OK; do not train on this
         
         self.vox_size_X = self.vox_util.default_vox_size_X
         self.vox_size_Y = self.vox_util.default_vox_size_Y
@@ -154,25 +152,25 @@ class CarlaDetModel(nn.Module):
         self.lrtlist_camX0s = __u(utils.geom.apply_4x4_to_lrtlist(__p(self.camX0s_T_camXs), __p(self.lrtlist_camXs)))
 
         # print('scorelist_s bef', self.scorelist_s.detach().cpu().numpy())
-        self.scorelist_s = __u(utils.misc.rescore_lrtlist_with_inbound(
-            __p(self.lrtlist_camRs), __p(self.tidlist_s), self.Z, self.Y, self.X, self.vox_util, pad=2.0))
+        inbound_s = __u(utils.misc.rescore_lrtlist_with_inbound(
+            __p(self.lrtlist_camRs), __p(self.tidlist_s), self.Z, self.Y, self.X, self.vox_util))
+        self.scorelist_s *= inbound_s
         # print('scorelist_s aft', self.scorelist_s.detach().cpu().numpy())
 
         for b in list(range(self.B)):
             if torch.sum(self.scorelist_s[:,0]) < (self.B/2): # not worth it; return early
-                # return 0.0, None, True
-                return False # not OK; do not try to train on this
+                return False # not OK; do not train on this
         
         self.rgb_camXs = feed['rgb_camXs']
         
-        self.summ_writer.summ_lrtlist(
-            'obj/box_camXs_g',
-            self.rgb_camXs[:,0],
-            self.lrtlist_camXs[:,0],
-            self.scorelist_s[:,0],
-            self.tidlist_s[:,0],
-            self.pix_T_cams[:,0])
-        # self.summ_writer.summ_rgbs('obj/box_camXs_g', visX_e)
+        # self.summ_writer.summ_lrtlist(
+        #     'obj/box_camXs_g',
+        #     self.rgb_camXs[:,0],
+        #     self.lrtlist_camXs[:,0],
+        #     self.scorelist_s[:,0],
+        #     self.tidlist_s[:,0],
+        #     self.pix_T_cams[:,0])
+        # # self.summ_writer.summ_rgbs('obj/box_camXs_g', visX_e)
         
         # get 3d voxelized inputs
         self.occ_memXs = __u(self.vox_util.voxelize_xyz(__p(self.xyz_camXs), self.Z, self.Y, self.X))
@@ -182,14 +180,14 @@ class CarlaDetModel(nn.Module):
         self.summ_writer.summ_occs('3d_inputs/occ_memXs', torch.unbind(self.occ_memXs, dim=1))
         self.summ_writer.summ_unps('3d_inputs/unp_memXs', torch.unbind(self.unp_memXs, dim=1), torch.unbind(self.occ_memXs, dim=1))
         
-        self.summ_writer.summ_lrtlist_bev(
-            'obj/bev_box_camXs_g',
-            self.occ_memXs[:,0],
-            self.lrtlist_camXs[:,0],
-            self.scorelist_s[:,0],
-            self.tidlist_s[:,0],
-            self.vox_util, 
-            already_mem=False)
+        # self.summ_writer.summ_lrtlist_bev(
+        #     'obj/bev_box_camXs_g',
+        #     self.occ_memXs[:,0],
+        #     self.lrtlist_camXs[:,0],
+        #     self.scorelist_s[:,0],
+        #     self.tidlist_s[:,0],
+        #     self.vox_util, 
+        #     already_mem=False)
 
         return True # OK
         
@@ -206,8 +204,11 @@ class CarlaDetModel(nn.Module):
 
         # eliminate the seq dim, to make life easier
         lrtlist_camX = self.lrtlist_camX0s[:, 0]
+        rgb_camX0 = self.rgb_camXs[:,0]
         occ_memX0 = self.occ_memXs[:,0]
         unp_memX0 = self.unp_memXs[:,0]
+        tidlist_g = self.tidlist_s[:,0]
+        scorelist_g = self.scorelist_s[:,0]
 
         if hyp.do_feat3d:
             # start with a 4-channel feature map;
@@ -222,14 +223,8 @@ class CarlaDetModel(nn.Module):
                 self.summ_writer)
             total_loss += feat3d_loss
 
-            valid_halfmemX0 = torch.ones_like(feat_halfmemX0[:,0:1])
-            feat_halfmemR0 = self.vox_util.apply_4x4_to_vox(self.camRs_T_camXs[:,0], feat_halfmemX0)
-            valid_halfmemR0 = self.vox_util.apply_4x4_to_vox(self.camRs_T_camXs[:,0], valid_halfmemX0)
-            # these are B x C x Z2 x Y2 x X2
-
             self.summ_writer.summ_feat('feat3d/feat_memX0_input', feat_memX0_input, pca=True)
             self.summ_writer.summ_feat('feat3d/feat_halfmemX0', feat_halfmemX0, pca=True)
-            self.summ_writer.summ_feat('feat3d/feat_halfmemR0', feat_halfmemR0, pca=True)
             
         if hyp.do_det:
 
@@ -241,37 +236,36 @@ class CarlaDetModel(nn.Module):
                 'det/boxlist_g',
                 occ_memX0[0:1],
                 lrtlist_memX[0:1],
-                torch.ones(1, 50).float().cuda(),  # scores
-                torch.ones(1, 50).long().cuda(),  # tids
+                scorelist_g,
+                tidlist_g, 
                 self.vox_util, 
                 already_mem=True)
             self.summ_writer.summ_lrtlist_bev(
                 'det/axboxlist_g',
                 occ_memX0[0:1],
                 axlrtlist_memX[0:1],
-                torch.ones(1, 50).float().cuda(),  # scores
-                torch.ones(1, 50).long().cuda(),  # tids
+                scorelist_g,
+                tidlist_g, 
                 self.vox_util, 
                 already_mem=True)
             
             lrtlist_halfmemX = self.vox_util.apply_mem_T_ref_to_lrtlist(lrtlist_camX, self.Z2, self.Y2, self.X2)
             axlrtlist_halfmemX = utils.geom.inflate_to_axis_aligned_lrtlist(lrtlist_halfmemX)
 
-            detect_loss, boxlist_halfmemX_e, scorelist_e, tidlist_e, pred_objectness, sco, ove = self.detnet(
+            det_loss, boxlist_halfmemX_e, scorelist_e, tidlist_e, pred_objectness, sco, ove = self.detnet(
                 axlrtlist_halfmemX,
-                self.scorelist_s[:, 0],
+                scorelist_g,
                 feat_halfmemX0,
                 self.summ_writer)
+            total_loss += det_loss
+            
             lrtlist_halfmemX_e = utils.geom.convert_boxlist_to_lrtlist(boxlist_halfmemX_e)
-            # print('lenlist_halfmem_e', utils.geom.get_lenlist_from_lrtlist(lrtlist_halfmemR_e))
             lrtlist_camX_e = self.vox_util.apply_ref_T_mem_to_lrtlist(lrtlist_halfmemX_e, self.Z2, self.Y2, self.X2)
-            # print('lenlist_cam_e', utils.geom.get_lenlist_from_lrtlist(lrtlist_camR_e))
-            total_loss += detect_loss
 
             lrtlist_e = lrtlist_camX_e[0:1]
             lrtlist_g = lrtlist_camX[0:1]
             scorelist_e = scorelist_e[0:1]
-            scorelist_g = self.scorelist_s[0:1, 0]
+            scorelist_g = scorelist_g[0:1]
             lrtlist_e, lrtlist_g, scorelist_e, scorelist_g = utils.eval.drop_invalid_lrts(
                 lrtlist_e, lrtlist_g, scorelist_e, scorelist_g)
 
@@ -282,84 +276,37 @@ class CarlaDetModel(nn.Module):
             axlenlist_g, _ = utils.geom.split_lrtlist(axlrtlist_camX[0:1])
             axclist_g = utils.geom.get_clist_from_lrtlist(axlrtlist_camX[0:1])
 
-            # if self.include_image_summs:
-            self.summ_writer.summ_lrtlist('obj/boxlist_e', self.rgb_camXs[:1, 0],
-                                          lrtlist_e,
-                                          scorelist_e,
-                                          tidlist_e, self.pix_T_cams[:1, 0])
             _, Ne, _ = list(lrtlist_e.shape)
             _, Ng, _ = list(lrtlist_g.shape)
-            # there may be no prediction or gt in the scene.
+            # only summ if there is at least one pred and one gt
             if Ne > 0 and Ng > 0:
                 lrtlist_e_ = lrtlist_e.unsqueeze(2).repeat(1, 1, Ng, 1).reshape(1, Ne * Ng, -1)
                 lrtlist_g_ = lrtlist_g.unsqueeze(1).repeat(1, Ne, 1, 1).reshape(1, Ne * Ng, -1)
                 ious, _ = utils.geom.get_iou_from_corresponded_lrtlists(lrtlist_e_, lrtlist_g_)
                 ious = ious.reshape(1, Ne, Ng)
                 ious_e = torch.max(ious, dim=2)[0]
-                self.summ_writer.summ_lrtlist('obj/boxlist', self.rgb_camXs[0:1, 0],
-                                              torch.cat((lrtlist_e, lrtlist_g), dim=1),
-                                              torch.cat((ious_e, ious_e.new_ones(1, Ng)), dim=1),
-                                              torch.cat((ious_e.new_ones(1, Ne).long(),
-                                                         ious_e.new_ones(1, Ng).long() * 2), dim=1),
+                self.summ_writer.summ_lrtlist('det/boxlist',
+                                              rgb_camX0[0:1],
+                                              torch.cat((lrtlist_g, lrtlist_e), dim=1),
+                                              torch.cat((ious_e.new_ones(1, Ng), ious_e), dim=1),
+                                              torch.cat([torch.ones(1, Ng).long().cuda(),
+                                                         torch.ones(1, Ne).long().cuda()+1], dim=1),
                                               self.pix_T_cams[0:1, 0])
-
-                self.summ_writer.summ_lrtlist_bev('det/boxlist_e', occ_memX0[:1],
-                                                  lrtlist_e,
-                                                  scorelist_e,
-                                                  tidlist_e,
-                                                  self.vox_util, 
-                                                  already_mem=False)
-                # visualize the gt and prediction on occ
-                self.summ_writer.summ_lrtlist_bev('det/boxlist', occ_memX0[0:1],
-                                                  torch.cat((lrtlist_e, lrtlist_g), dim=1),
-                                                  torch.cat((ious_e, ious_e.new_ones(1, Ng)), dim=1),
-                                                  torch.cat((ious_e.new_ones(1, Ne).long(),
-                                                             ious_e.new_ones(1, Ng).long() * 2), dim=1),
+                self.summ_writer.summ_lrtlist_bev('det/boxlist_bev',
+                                                  occ_memX0[0:1],
+                                                  torch.cat((lrtlist_g, lrtlist_e), dim=1),
+                                                  torch.cat((ious_e.new_ones(1, Ng), ious_e), dim=1),
+                                                  torch.cat([torch.ones(1, Ng).long().cuda(),
+                                                             torch.ones(1, Ne).long().cuda()+1], dim=1),
                                                   self.vox_util, 
                                                   already_mem=False)
 
             ious = [0.3, 0.4, 0.5, 0.6, 0.7]
             maps_3d, maps_2d = utils.eval.get_mAP_from_lrtlist(lrtlist_e, scorelist_e, lrtlist_g, ious)
             for ind, overlap in enumerate(ious):
-                self.summ_writer.summ_scalar('ap/%.2f_iou' % overlap, maps_3d[ind])
+                self.summ_writer.summ_scalar('ap_3d/%.2f_iou' % overlap, maps_3d[ind])
+                self.summ_writer.summ_scalar('ap_bev/%.2f_iou' % overlap, maps_2d[ind])
 
-            
-            # axboxlist_camRs = __u(utils.geom.inflate_to_axis_aligned_boxlist(__p(self.boxlist_camRs)))
-            # axlrtlist_camRs = __u(utils.geom.convert_boxlist_to_lrtlist(__p(axboxlist_camRs)))
-            # if self.include_image_summs:
-            #     self.summ_writer.summ_lrtlist('obj/axboxlist', self.rgb_camRs[:,0], axlrtlist_camRs[:,0],
-            #                              self.scorelist_s[:,0], self.tidlist_s[:,0], self.pix_T_cams[:,0])
-
-            # boxlist_memR = utils.vox.convert_boxlist_camR_to_memR(self.boxlist_camRs[:,0], self.Z2, self.Y2, self.X2)
-            # axboxlist_memR = utils.geom.inflate_to_axis_aligned_boxlist(boxlist_memR)
-
-            # featRs = utils.vox.apply_4x4s_to_voxs(self.camRs_T_camXs, feat_memXs)
-            # featR = torch.mean(featRs, dim=1)
-
-            # detect_loss, boxlist_memR_e, scorelist_e, tidlist_e, sco, ove = self.detnet(
-            #     axboxlist_memR,
-            #     self.scorelist_s[:,0],
-            #     featR,
-            #     self.summ_writer)
-            # total_loss += detect_loss
-
-            # boxlist_camR_e = utils.vox.convert_boxlist_memR_to_camR(boxlist_memR_e, self.Z2, self.Y2, self.X2)
-            # lrtlist_camR_e = utils.geom.convert_boxlist_to_lrtlist(boxlist_camR_e)
-            # if self.include_image_summs:
-            #     self.summ_writer.summ_lrtlist('obj/boxlist_e', self.rgb_camRs[:,0], lrtlist_camR_e,
-            #                              scorelist_e, tidlist_e, self.pix_T_cams[:,0])
-
-            # boxlist_e = boxlist_camR_e[0:1].detach().cpu().numpy()
-            # boxlist_g = self.boxlist_camRs[0:1,0].detach().cpu().numpy()
-            # scorelist_e = scorelist_e[0:1].detach().cpu().numpy()
-            # scorelist_g = self.scorelist_s[0:1,0].detach().cpu().numpy()
-            # boxlist_e, boxlist_g, scorelist_e, scorelist_g = utils.eval.drop_invalid_boxes(
-            #     boxlist_e, boxlist_g, scorelist_e, scorelist_g)
-
-            # ious = [0.3, 0.4, 0.5, 0.6, 0.7]
-            # maps = utils.eval.get_mAP(boxlist_e, scorelist_e, boxlist_g, ious)
-            # for ind, overlap in enumerate(ious):
-            #     self.summ_writer.summ_scalar('ap/%.2f_iou' % overlap, maps[ind])
             
         self.summ_writer.summ_scalar('loss', total_loss.cpu().item())
         return total_loss, results, False
